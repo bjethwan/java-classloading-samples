ef87eddc2914a84e647c73d5683773ff807b0da2
Bipin Jethwani <jethwani.bipin@gmail.com>
2016-03-05 21:20:10 +0530

    initial commit - sample1 loading classes from jar on cmd line

---
+++ sample1/src/com/bipin/Main.java
@@ -0,0 +1,8 @@
+package com.bipin;
+
+public class Main {
+	public static void main(String[] args) {
+		com.jethwani.Helper helper = new com.jethwani.Helper();
+		System.out.println(helper.getMessage());
+	}
+}
---
+++ sample1/src/com/jethwani/Helper.java
@@ -0,0 +1,7 @@
+package com.jethwani;
+
+public class Helper {
+	public String getMessage(){
+		return "One small step for man";
+	}
+}
